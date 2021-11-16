5e423c258f067f2dc5b2f30527309d053474057f
Your Name <you@example.com>
2021-11-16 13:41:21 +0100

    Version_16.11.2021_13:41

---
+++ Aufgabe_2_monatliche_Zahlung.py
@@ -0,0 +1,11 @@
+# Name   : Christian Meier
+# Klasse : ETS 2021
+# Datum  : 16.11.2021
+
+Krieditsumme    = 6000
+Zinsatz         = 19
+Jahre           = 10
+
+monatlicheRate = Jahre
+
+print(monatlicheRate ,"€ werden monatlich Bezahlt werden")
